ec5962a20a64933aa4ee1d694a2e15b86fb03bd8
Stéphan Kochen <git@stephank.nl>
2020-06-06 21:48:20 +0200

    Simplify and speed up build

--- build.sh
+++ build.sh
@@ -3,31 +3,17 @@ set -xeuo pipefail
 
 cd "$(dirname "$0")"
 
-# Remove previous output files. Many of these may not have write permissions,
-# which prevents us from deleting them. That's why we chmod.
-if [ -d output ]; then
-  chmod -R u+w output
-  rm -fr output
-fi
-
-# Add the base system configuration to NIX_PATH.
+# Add the base system config to the start of NIX_PATH.
 export NIX_PATH="darwin-config=${PWD}/support/base-system-config.nix:${NIX_PATH}"
 
-# Build the nix-darwin system from the dummy config.
+# Build the base system.
 sys_build="$(nix-build '<darwin>' --attr system --no-out-link)"
 
-# Get the closure for the entire system.
+# Get the closure for the entire base system.
 sys_closure="$(nix-store --query --requisites "${sys_build}")"
-
-# Prepare the output directory.
-mkdir -p output/store
-cp -r support/static/* output/
 
 # Create a Nix database dump for the closure.
 nix-store --dump-db ${sys_closure} > output/.reginfo
-
-# Copy the closure to the output directory.
-cp -R ${sys_closure} output/store/
 
 # Build the installer script.
 sed -e "s|@@sys@@|${sys_build}|g" \
@@ -35,8 +21,9 @@ sed -e "s|@@sys@@|${sys_build}|g" \
   > output/install.sh
 chmod a+x output/install.sh
 
-# Create the output tarball.
+# Create a tarball containing the output plus the closure.
 tar --create --auto-compress --numeric-owner --uid 0 --gid 0 \
   --file "nix-darwin-installer.tar.xz" \
-  -s '/^output/nix-darwin-installer/' \
-  output
+  -s '|^/nix|nix-darwin-installer|S' \
+  -s '|^output|nix-darwin-installer|' \
+  ${sys_closure} output
